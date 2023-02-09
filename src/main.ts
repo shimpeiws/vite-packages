27144efca7994f6b9059e3dab1538f9f0c272ac1
shimpeiws <shimpeiws@gmail.com>
2023-02-10 04:42:44 +0900

    Revert "open-cv"
    This reverts commit a955d351d147b838b0f9a9ed0bf84e6b0736345c.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,5 @@
 import './style.css'
 import { readFile } from '../lib/main'
-import cv, { Mat, Rect } from "opencv-ts";
-
-cv.onRuntimeInitialized = () => {
-  const src = cv.imread("inputCanvas");
-  const dst: Mat = new cv.Mat(src.cols, src.rows, cv.CV_8UC4);
-
-  cv.resize(src, dst, new cv.Size(500, 500), 0, 0, cv.INTER_AREA);
-
-  const roiRect: Rect = new cv.Rect(0, 0, 200, 200);
-
-  const roi = dst.roi(roiRect);
-
-  cv.imshow('outputCanvas', roi);
-};
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
